e20c5736e2bcfc6980c966f19f08889a42776dcb
Someone <120342697+Somespi@users.noreply.github.com>
2023-05-25 02:23:54 +0400

    Update Punic.h

--- Punic.h
+++ Punic.h
@@ -17,14 +17,44 @@ private:
     bool exiting = false;
 
 public:
-    ~Punic();
+    ~Punic() {
+        summary();
+    }
 
     template <typename Function>
-    bool assert(auto assertion, Function func, std::string named = "Unnamed");
+    bool assert(auto assertion, Function func, std::string named = "Unnamed") {
+        tests += 1;
+        auto returns = func();
+        bool result = returns == assertion;
 
-    void exitFailure(bool on_exit);
+        std::cout << BOLD << "Test <" << YELLOW << named << WHITE << "> #" << tests << " : " << (result ? GREEN : RED) << (result ? "PASSED" : "FAILED") << RESET << std::endl;
+
+        if (!result) {
+            failures += 1;
+            std::cout << "  - " << YELLOW << "Expected: " << MAGENTA << assertion << RESET << std::endl;
+            std::cout << "  - " << YELLOW << "Actual:   " << MAGENTA << returns << RESET << std::endl;
+
+        }
+
+        if (!result && exiting) {
+            std::cerr << RED << "Test failure. Exiting the program." << RESET << std::endl;
+            std::exit(1);
+        }
+
+        return result;
+    }
+
+    void exitFailure(bool on_exit) {
+        exiting = on_exit;
+    }
 
 private:
-    void summary();
-};
+    void summary() {
+        if (failures == 0) {
+            std::cout << GREEN << "All tests passed." << RESET << std::endl;
+        }
+        else {
+            std::cout << YELLOW << "Tests passed with " << RED << failures << YELLOW << " fail(s)." << RESET << std::endl;
+        }
+}
 #endif
